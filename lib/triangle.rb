08e70ce13df4ed4b22244ba7106a9031055b15c0
Chloe Scheffler <chloescheffler@gmail.com>
2022-10-16 22:13:06 -0400

    commit tree b42078e39d026ca4e71b96b29967f83481e627f7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind 
+
+  end
 end
+
+binding.pry
